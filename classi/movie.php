e83da9aa15bbabf402ec54239e0c7b6b9521067e
Gabriele Mainetti <mg02.gabriele@gmail.com>
2022-12-20 17:34:53 +0100

    creati attributi e condizione

---
+++ classi/movie.php
@@ -0,0 +1,24 @@
+<?php
+
+class Movie{
+    //attributi
+    public $title;
+    public $genre;
+    public $year;
+    public $age;
+
+    public function __construct($title, $genre, $year, $age)
+    {
+        $this -> title = $title;
+        $this -> genre = $title;
+        $this -> year = $title;
+        $this -> year = $age;
+    }
+
+    public function metodo(){
+        if($this->age)
+    }
+
+}
+
+?>
